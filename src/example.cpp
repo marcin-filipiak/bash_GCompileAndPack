ee81d05ba1a98b2d75fd8881f669b04e3740940c
marcin-filipiak <m.filipiak@noweenergie.org>
2024-03-22 13:46:48 +0100

    first commit

---
+++ src/example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define VERSION "0.1"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    std::cout << "Example app compiled in GCompileAndPack" << std::endl;
+    return 0;
+}
+
